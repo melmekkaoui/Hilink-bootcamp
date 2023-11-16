51061ce90c33a218cffb5c6c538d6760ef43e5b6
ELMEKKAOUI <mohammed.elmekkaoui27@gmail.com>
2023-11-17 00:36:48 +0100

    first commit

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="border bg-white py-20">
+        <p className="regular-14 w-full text-center text-gray-30">2023 Hilink | All rights reserved</p>
+     </div>
+  )
+}
+
+export default Footer
